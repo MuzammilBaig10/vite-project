f1a8190e7c58b795564c0feacbad708a91366b17
MuzammilBaig10 <mirzabaig4689@gmail.com>
2025-01-19 14:59:15 +0500

    icons, buttons, components,

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { FaUserSecret } from "react-icons/fa";
+import ButtonCustom from "./components/ButtonCustom";
+import './App.css'
+
 const App = () => {
   return (
     <>
     <div>Name: Muzammil Baig</div>
+    <FaUserSecret size={500} />
     <div>Age: 20</div>
+    <ButtonCustom/>
     </>
   )
 }
---
+++ src/components/ButtonCustom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonCustom = () => {
+  return (
+    <button>Text Here</button>
+  )
+}
+
+export default ButtonCustom
